37be0ae5129e5ef7feca64f7a56184353ba5551f
Anupam <anupam.r2301@gmail.com>
2026-06-24 22:19:45 +0530

    fixing deployment

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ import dns from "dns"
 import cors from "cors"
 import authRoutes from "./src/routes/auth.routes.js"
 import userRoutes from "./src/routes/user.routes.js"
-import kycRoutes from  "./src/routes/kyc.routes.js"
+import kycRoutes from "./src/routes/kyc.routes.js"
 import bankAccountRoutes from "./src/routes/bankAccount.routes.js"
 import transactionRoutes from "./src/routes/transaction.routes.js"
 import beneficairyRoutes from "./src/routes/beneficairy.routes.js"
@@ -15,25 +15,31 @@ import errorMiddleware from "./src/middlewares/error.middleware.js"
 import dotenv from "dotenv"
 dotenv.config()
 
-dns.setServers(["1.1.1.1","0.0.0.0"])
+dns.setServers(["1.1.1.1", "0.0.0.0"])
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Backend is running successfully 🚀");
-});
-
+  res.status(200).json({
+    success: true,
+    project: "Digital Payment Platform API",
+    status: "Running",
+    documentation: "/api-docs",
+    message:
+      "Append '/api-docs' to this URL to view and test the API documentation using Swagger UI."
+  })
+})
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
 
-app.use("/api/auth",authRoutes)
-app.use("/api/users",userRoutes)
-app.use("/api/kyc",kycRoutes)
-app.use("/api/bank",bankAccountRoutes)
-app.use("/api/transaction",transactionRoutes)
-app.use("/api/beneficairy",beneficairyRoutes)
-app.use("/api/notification",notificationRoutes)
-app.use("/api/accountLedger",accountLedgerRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/kyc", kycRoutes)
+app.use("/api/bank", bankAccountRoutes)
+app.use("/api/transaction", transactionRoutes)
+app.use("/api/beneficairy", beneficairyRoutes)
+app.use("/api/notification", notificationRoutes)
+app.use("/api/accountLedger", accountLedgerRoutes)
 
 app.use(
   "/api-docs",
